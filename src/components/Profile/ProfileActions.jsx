48ebd46ac11e17702e71cf9f44345ec382cae2d5
Carl Markus Malde <carl.markus.malde@no.experis.com>
2022-09-23 14:49:24 +0200

    added function to clear translation history

--- src/components/Profile/ProfileActions.jsx
+++ src/components/Profile/ProfileActions.jsx
@@ -1,26 +1,51 @@
-import { STORAGE_KEY_USER } from "../../const/storageKeys"
-import { useUser } from "../../context/UserContext"
-import { storageDelete } from "../../utils/storage"
+import { clearTranslation } from "../../api/translate";
+import { STORAGE_KEY_USER } from "../../const/storageKeys";
+import { useUser } from "../../context/UserContext";
+import { storageDelete, storageSave } from "../../utils/storage";
 
 const ProfileActions = () => {
+  const { user, setUser } = useUser();
 
-    const { setUser } = useUser()
-
-    const handleLogoutClick = () => {
-        if (window.confirm('Are you sure?')){
-            storageDelete(STORAGE_KEY_USER)
-            setUser(null)
-        }
+  const handleLogoutClick = () => {
+    if (window.confirm("Are you sure?")) {
+      storageDelete(STORAGE_KEY_USER);
+      setUser(null);
     }
+  };
 
-    
+  const clearHistoryClick = async () => {
+    if (!window.confirm("Are you sure you want to clear your history?")) {
+      return;
+    }
+    const [clearError] = await clearTranslation(user.id);
 
-    return (
-        <ul id="ulProfile">
-            <li><button id="btnLogout" onClick={ handleLogoutClick }>Logout</button></li>
-            <li><button id="btnClear">Clear history</button></li>
-        </ul>
-    )
-}
+    if (clearError != null) {
+      console.log("error");
+      return;
+    }
+    const updatedUser = {
+      ...user,
+      translations: [],
+    };
 
-export default ProfileActions
+    storageSave(updatedUser);
+    setUser(updatedUser);
+  };
+
+  return (
+    <ul id="ulProfile">
+      <li>
+        <button id="btnLogout" onClick={handleLogoutClick}>
+          Logout
+        </button>
+      </li>
+      <li>
+        <button onClick={clearHistoryClick} id="btnClear">
+          Clear history
+        </button>
+      </li>
+    </ul>
+  );
+};
+
+export default ProfileActions;
